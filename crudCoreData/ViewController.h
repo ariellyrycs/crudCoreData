86daa7194baf17c783fe27356afa8108cec1c58b
ariellyrycs <ariel_556@hotmail.com>
2015-03-01 11:52:30 -0700

    first commit

---
+++ crudCoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  crudCoreData
+//
+//  Created by Ariel Robles on 3/1/15.
+//  Copyright (c) 2015 Ariel Robles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ crudCoreData/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  crudCoreData
+//
+//  Created by Ariel Robles on 3/1/15.
+//  Copyright (c) 2015 Ariel Robles. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
